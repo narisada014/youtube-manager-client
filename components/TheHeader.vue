5f3b1dba0956eaf351abff5f89e74f28b3749d54
narisada014 <nari.reishi@gmail.com>
2019-10-15 23:59:11 +0900

    add youtube player base

---
+++ components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <nuxt-link class="navbar-item subtitle is-4" to="/">
+        Youtube Player
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "TheHeader"
+  }
+</script>
+
+<style scoped>
+
+</style>
